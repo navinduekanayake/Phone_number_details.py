788bb9cd93757a0018c1127858e412a3cab8f386
Navindu Ekanyake <98044666+navinduekanayake@users.noreply.github.com>
2022-01-27 15:44:36 +0530

    Update phone_number.py

--- phone_number.py
+++ phone_number.py
@@ -3,7 +3,7 @@ from phonenumbers import carrier
 from phonenumbers import geocoder
 from phonenumbers import timezone
 
-number = "+94711660640"
+number = "your phone number"
 number = ph.parse(number)
 print(timezone.time_zones_for_number(number))
 print(carrier.name_for_number(number, "en"))
